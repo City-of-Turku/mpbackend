d7e49a2d2680dddad341f68241b9f008cd7a5b1c
juuso-j <juuso.jokiniemi@turku.fi>
2023-04-20 14:58:41 +0300

    Register profiles urls to ^api/v1/

--- mpbackend/urls.py
+++ mpbackend/urls.py
@@ -1,22 +1,24 @@
-"""
-URL configuration for mpbackend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, re_path
+from rest_framework import routers
+
+from profiles.api.views import all_views
+
+router = routers.DefaultRouter()
+registered_api_views = set()
+for view in all_views:
+    kwargs = {}
+    if view["name"] in registered_api_views:
+        continue
+    else:
+        registered_api_views.add(view["name"])
+
+    if "basename" in view:
+        kwargs["basename"] = view["basename"]
+    router.register(view["name"], view["class"], **kwargs)
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    re_path("^admin/", admin.site.urls),
+    re_path(r"^api/v1/", include(router.urls)),
 ]
