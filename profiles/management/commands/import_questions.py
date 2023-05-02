e39e5a42c1776897e8ed57685da67054fbffea93
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-02 14:42:32 +0300

    Add question if changed, delete obsolete

--- profiles/management/commands/import_questions.py
+++ profiles/management/commands/import_questions.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any
 
 import pandas as pd
 from django.conf import settings
@@ -6,12 +7,12 @@ from django.core.management import BaseCommand
 
 from profiles.models import Option, Question, Result, SubQuestion
 
-
 FILENAME = "prof2.xlsx"
 IS_ANIMAL = 1
 
 LANGUAGES = [language[0] for language in settings.LANGUAGES]
 LANGUAGE_SEPARATOR = "/"
+QUESTION_NUMBER_COLUMN = 0
 QUESTION_COLUMN = 1
 QUESTION_DESCRIPTION_COLUMN = 4
 SUB_QUESTION_COLUMN = 5
@@ -20,7 +21,7 @@ SUB_QUESTION_DESCRIPTION_COLUMN = 7
 OPTION_COLUMN = 8
 
 
-def get_root_dir():
+def get_root_dir() -> str:
     """
     Returns the root directory of the project.
     """
@@ -30,7 +31,7 @@ def get_root_dir():
         return settings.BASE_DIR
 
 
-def get_or_create_results(data):
+def get_or_create_results(data: pd.DataFrame) -> list:
     Result.objects.all().delete()
     columns = data.columns[9:15]
     results = []
@@ -43,7 +44,7 @@ def get_or_create_results(data):
     return results
 
 
-def save_translated_field(obj, field_name, data):
+def save_translated_field(obj: Any, field_name: str, data: dict):
     """
     Sets the value of all languages for given field_name.
     :param obj: the object to which the fields will be set
@@ -58,7 +59,7 @@ def save_translated_field(obj, field_name, data):
     obj.save()
 
 
-def get_language_dict(data):
+def get_language_dict(data: str) -> dict:
     data = str(data).split(LANGUAGE_SEPARATOR)
     d = {}
     for i, lang in enumerate(LANGUAGES):
@@ -81,34 +82,41 @@ class Command(BaseCommand):
         # excel_data = excel_data.fillna(None)
         excel_data = excel_data.fillna("").replace([""], [None])
         results = get_or_create_results(excel_data.iloc[0:3])
-        Question.objects.all().delete()
         question = None
         sub_question = None
         num_rows = 0
-        question_description_created = False
+        num_created = 0
+        questions_to_delete = list(Question.objects.all().values_list("id", flat=True))
         for row in excel_data.iterrows():
-            num_rows += 1
-
             # TODO, remove when excel file changes are complete
+            num_rows += 1
             if num_rows > 226:
-                return
-
+                break
             row_data = row[1]
             try:
-                col_0 = str(row_data[0])
+                question_number = str(row_data[QUESTION_NUMBER_COLUMN])
             except TypeError:
                 continue
 
-            if col_0[0].isdigit():
-                question = Question.objects.create()
-                q_str = row_data[QUESTION_COLUMN]
+            if question_number[0].isdigit():
+                questions = get_language_dict(row_data[QUESTION_COLUMN])
+                descriptions = get_language_dict(row_data[QUESTION_DESCRIPTION_COLUMN])
+                filter = {"number": question_number}
+                for lang in LANGUAGES:
+                    filter[f"question_{lang}"] = questions[lang]
+                    filter[f"description_{lang}"] = descriptions[lang]
 
-                save_translated_field(question, "question", get_language_dict(q_str))
-                desc_str = row_data[QUESTION_DESCRIPTION_COLUMN]
-                if desc_str:
-                    save_translated_field(
-                        question, "description", get_language_dict(desc_str)
-                    )
+                queryset = Question.objects.filter(**filter)
+                if queryset.count() == 0:
+                    question = Question.objects.create(**filter)
+                    num_created += 1
+                else:
+                    if queryset.count() > 1:
+                        print(f"Found duplicate MobileUnit {filter}")
+                    question = queryset.first()
+                    id = queryset.first().id
+                    if id in questions_to_delete:
+                        questions_to_delete.remove(id)
 
                 sub_question = None
 
@@ -129,7 +137,6 @@ class Command(BaseCommand):
                     )
 
             # Create option
-
             if question or sub_question and row_data[OPTION_COLUMN]:
                 if sub_question:
                     option = Option.objects.create(sub_question=sub_question)
@@ -141,3 +148,6 @@ class Command(BaseCommand):
                 for a_i, a_c in enumerate(result_columns):
                     if row_data[a_c] == IS_ANIMAL:
                         option.results.add(results[a_i])
+        Question.objects.filter(id__in=questions_to_delete).delete()
+        print(questions_to_delete)
+        print(f"Created {num_created} questions")
