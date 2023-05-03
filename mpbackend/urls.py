3e1406c39acf4ea8489dd1d251f3c90843c07658
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-03 16:23:45 +0300

    Import urls from profiles.api

--- mpbackend/urls.py
+++ mpbackend/urls.py
@@ -1,24 +1,11 @@
 from django.contrib import admin
-from django.urls import include, re_path
-from rest_framework import routers
+from django.urls import include, path, re_path
 
-from profiles.api.views import all_views
-
-router = routers.DefaultRouter()
-registered_api_views = set()
-for view in all_views:
-    kwargs = {}
-    if view["name"] in registered_api_views:
-        continue
-    else:
-        registered_api_views.add(view["name"])
-
-    if "basename" in view:
-        kwargs["basename"] = view["basename"]
-    router.register(view["name"], view["class"], **kwargs)
-
+import profiles.api.urls
 
 urlpatterns = [
     re_path("^admin/", admin.site.urls),
-    re_path(r"^api/v1/", include(router.urls)),
+    re_path(r"^api/v1/", include(profiles.api.urls), name="profiles"),
+    # TODO, remove
+    path("api-auth/", include("rest_framework.urls")),
 ]
