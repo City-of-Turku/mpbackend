df97c8dff9fa8d854b916ad76966001d3d6ecacd
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-04 14:52:19 +0300

    Add tests for get-result, end poll and get question by number

--- profiles/tests/test_api.py
+++ profiles/tests/test_api.py
@@ -28,3 +28,25 @@ def test_poll(api_client, questions, options, results):
     assert Answer.objects.count() == 1
     user = User.objects.first()
     assert user.result.value == "negative result"
+    # Test get-result endpoint
+    url = reverse("profiles:answer-get-result")
+    response = api_client.get(url)
+    assert response.status_code == 200
+    assert response.json()["value"] == "negative result"
+    # Test end poll (logout)
+    url = reverse("profiles:question-end-poll")
+    response = api_client.get(url)
+    assert response.status_code == 200
+    url = reverse("profiles:answer-list")
+    # should return 403 Forbidden
+    response = api_client.post(url, {"option": option.id})
+    assert response.status_code == 403
+    # Test get_question by its number
+    url = reverse("profiles:question-get-question") + "?number=2"
+    response = api_client.get(url)
+    assert response.status_code == 200
+    assert response.json()["question"] == "Do you use scooter?"
+    # Test getting nonexisting question by its number
+    url = reverse("profiles:question-get-question") + "?number=2222"
+    response = api_client.get(url)
+    assert response.status_code == 404
