c0ca10fe103b1611626abe6118271f1c9321a8ab
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-04 14:49:14 +0300

    Add generate_password function

--- profiles/utils.py
+++ profiles/utils.py
@@ -1,3 +1,6 @@
+import secrets
+import string
+
 from account.models import User
 from profiles.models import Answer, Result
 
@@ -13,3 +16,21 @@ def get_user_result(user: User) -> Result:
             results[result] += 1
     result = max(results, key=results.get)
     return result
+
+
+def generate_password() -> str:
+    """
+    https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
+    Generate a 10 alphanumeric password with at least one lowercase character,
+      at least one uppercase character, and at least three digits:
+    """
+    alphabet = string.ascii_letters + string.digits
+    while True:
+        password = "".join(secrets.choice(alphabet) for i in range(10))
+        if (
+            any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password) >= 3
+        ):
+            break
+    return password
