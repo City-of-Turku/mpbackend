06d40036bea80c7a493a906a540bcfaf45d603e5
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-05 13:47:21 +0300

    Add tests for questions API

--- profiles/tests/test_api.py
+++ profiles/tests/test_api.py
@@ -41,12 +41,43 @@ def test_poll(api_client, questions, options, results):
     # should return 403 Forbidden
     response = api_client.post(url, {"option": option.id})
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_questions(api_client, questions, options, results):
+    # Test questions list
+    url = reverse("profiles:question-list")
+    response = api_client.get(url)
+    assert len(response.json()["results"]) == questions.count()
+    question = questions.first()
+    url = reverse("profiles:question-detail", args=[str(question.id)])
+    response = api_client.get(url)
+    json_response = response.json()
+    assert json_response["question"] == question.question
+    assert len(json_response["options"]) == options.filter(question=question).count()
+    assert (
+        json_response["options"][0]["value"]
+        == options.filter(question=question).first().value
+    )
+    assert (
+        len(json_response["options"][0]["results"])
+        == question.options.first().results.count()
+    )
+    assert (
+        json_response["options"][0]["results"][0]["value"]
+        == results.filter(options__question=question).first().value
+    )
     # Test get_question by its number
     url = reverse("profiles:question-get-question") + "?number=2"
     response = api_client.get(url)
     assert response.status_code == 200
-    assert response.json()["question"] == "Do you use scooter?"
+    assert response.json()["question"] == questions.filter(number=2).first().question
     # Test getting nonexisting question by its number
     url = reverse("profiles:question-get-question") + "?number=2222"
     response = api_client.get(url)
     assert response.status_code == 404
+    # Test get_question_number
+    url = reverse("profiles:question-get-question-numbers")
+    response = api_client.get(url)
+    assert response.status_code == 200
+    assert response.json()["results"][-1]["number"] == questions.last().number
