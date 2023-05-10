aa7bb0076a6c8077ddc318927ad1b409fd997b5a
juuso-j <juuso.jokiniemi@turku.fi>
2023-05-10 13:55:24 +0300

    Add postal_code and optional_postal_code

--- account/models.py
+++ account/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     result = models.ForeignKey(
         "profiles.Result",
         related_name="users",
@@ -34,6 +37,8 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
     )
     gender = models.CharField(max_length=2, choices=GENDER_OPTIONS, blank=True)
+    postal_code = models.CharField(max_length=10, null=True)
+    optional_postal_code = models.CharField(max_length=10, null=True)
 
     def __str__(self):
         return self.user.username
