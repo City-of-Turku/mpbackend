c84417fcf260dd49ac94e3934f8858e62ce1a767
juuso-j <juuso.jokiniemi@turku.fi>
2023-09-20 10:24:09 +0300

    Add number_of_choices, QuestionCondition model and str functions

--- profiles/models.py
+++ profiles/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 
 
-class QuestionCondition(models.Model):
-    pass
-
-
 class Question(models.Model):
     number = models.CharField(max_length=3, null=True)
     question = models.CharField(max_length=255, null=True)
     description = models.CharField(max_length=255, null=True)
-    question_condition = models.OneToOneField(
-        "QuestionCondition",
-        related_name="question",
-        null=True,
-        on_delete=models.CASCADE,
-    )
-    number_of_choices = models.PositiveSmallIntegerField(default=1)
+    number_of_choices = models.CharField(max_length=2, default="1")
+
     number_of_sub_question_choices = models.PositiveSmallIntegerField(default=1)
 
     class Meta:
         ordering = ["number"]
+
+    def __str__(self):
+        return f"question number:{self.number}, question: {self.question}"
 
 
 class SubQuestion(models.Model):
@@ -28,9 +22,13 @@ class SubQuestion(models.Model):
     question = models.ForeignKey(
         "Question", related_name="sub_questions", null=True, on_delete=models.CASCADE
     )
+    order_number = models.PositiveSmallIntegerField(null=True)
 
     class Meta:
         ordering = ["question__number"]
+
+    def __str__(self):
+        return f"{self.description}"
 
 
 class Option(models.Model):
@@ -43,20 +41,25 @@ class Option(models.Model):
         "SubQuestion", related_name="options", on_delete=models.CASCADE, null=True
     )
     results = models.ManyToManyField("Result", related_name="options")
-    question_condition = models.ForeignKey(
-        "QuestionCondition", related_name="options", on_delete=models.CASCADE, null=True
-    )
+    order_number = models.PositiveSmallIntegerField(null=True)
 
     class Meta:
         ordering = ["question__number", "sub_question__question__number"]
 
+    def __str__(self):
+        return f"Value: {self.value}"
+
 
 class Result(models.Model):
+    topic = models.CharField(max_length=64, null=True)
     value = models.CharField(max_length=64, null=True)
     description = models.TextField(null=True)
 
     class Meta:
         ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.topic} / {self.value}"
 
 
 class Answer(models.Model):
@@ -75,3 +78,28 @@ class Answer(models.Model):
             )
         ]
         ordering = ["id"]
+
+
+class QuestionCondition(models.Model):
+    question = models.ForeignKey(
+        "Question",
+        related_name="conditions",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    question_condition = models.ForeignKey(
+        "Question",
+        related_name="question_conditions",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    sub_question_condition = models.ForeignKey(
+        "SubQuestion",
+        related_name="sub_question_conditions",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    option_conditions = models.ManyToManyField(
+        "Option", related_name="option_conditions"
+    )
