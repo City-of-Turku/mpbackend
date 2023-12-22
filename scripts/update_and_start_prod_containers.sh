ea29db1e55c6366217f227cc0081bdac1b5dd355
juuso-j <juuso.jokiniemi@turku.fi>
2023-12-22 10:39:33 +0200

    Set relative location of docker-compose files

--- scripts/update_and_start_prod_containers.sh
+++ scripts/update_and_start_prod_containers.sh
@@ -9,5 +9,5 @@ docker system prune -a -f
 docker volume rm mpbackend_static
 docker volume rm mpbackend_mpbackend
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml start
+docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml up
+docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml start
