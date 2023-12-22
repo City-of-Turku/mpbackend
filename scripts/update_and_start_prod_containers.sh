448244efab1496e5a43e9e5789caf082fa97e025
juuso-j <juuso.jokiniemi@turku.fi>
2023-12-22 10:35:00 +0200

    Add script for updating and starting production containers

---
+++ scripts/update_and_start_prod_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add git checkout
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -aq) 
+docker system prune -a -f
+# Remove all volumes, Except for mpbackend_postrgres-data as it contains the database
+docker volume rm mpbackend_static
+docker volume rm mpbackend_mpbackend
+
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml start
