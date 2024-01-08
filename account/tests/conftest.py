1f5aa378e38ecd7c6a47dd05ebb594d52e5d5d7a
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-08 15:19:27 +0200

    Add basic users and profiles fixtures

--- account/tests/conftest.py
+++ account/tests/conftest.py
@@ -1,7 +1,22 @@
 import pytest
 from rest_framework.test import APIClient
 
+from account.models import Profile, User
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def users():
+    User.objects.create(username="test1")
+    return User.objects.all()
+
+
+@pytest.fixture
+def profiles(users):
+    test_user = User.objects.get(username="test1")
+    Profile.objects.create(user=test_user)
+    return Profile.objects.all()
