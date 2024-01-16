32bb021a86abd9d2ecc83dc8dba9b2cade352db8
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-16 10:17:03 +0200

    Add results and mailing_lists fixtures

--- account/tests/conftest.py
+++ account/tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from rest_framework.test import APIClient
 
-from account.models import Profile, User
+from account.models import MailingList, MailingListEmail, Profile, User
+from profiles.models import Result
 
 
 @pytest.fixture
@@ -20,3 +21,27 @@ def profiles(users):
     test_user = User.objects.get(username="test1")
     Profile.objects.create(user=test_user)
     return Profile.objects.all()
+
+
+@pytest.fixture
+def results():
+    Result.objects.create(topic="Car traveller")
+    Result.objects.create(topic="Habit traveller")
+    return Result.objects.all()
+
+
+@pytest.fixture
+def mailing_lists(results):
+    MailingList.objects.create(result=results.first())
+    return MailingList.objects.all()
+
+
+@pytest.fixture
+def mailing_list_emails(mailing_lists):
+    MailingListEmail.objects.create(
+        email="test@test.com", mailing_list=mailing_lists.first()
+    )
+    MailingListEmail.objects.create(
+        email="abc@efg.com", mailing_list=mailing_lists.first()
+    )
+    return MailingListEmail.objects.all()
