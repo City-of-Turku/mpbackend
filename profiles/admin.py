4708bfa0c92eeb5847f91a2568059cfaf266206f
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-16 15:52:32 +0200

    Restrict permissions to change/delete rows in models

--- profiles/admin.py
+++ profiles/admin.py
@@ -10,14 +10,87 @@ from profiles.models import (
     QuestionCondition,
     Result,
     SubQuestion,
+    SubQuestionCondition,
 )
 
-admin.site.register(Question)
-admin.site.register(QuestionCondition)
-admin.site.register(Option)
-admin.site.register(SubQuestion)
-admin.site.register(Result)
-admin.site.register(Answer)
-admin.site.register(PostalCode)
-admin.site.register(PostalCodeType)
-admin.site.register(PostalCodeResult)
+
+class ReadOnlyFieldsAdminMixin:
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class DisableDeleteAdminMixin:
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class QuestionAdmin(DisableDeleteAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Question
+
+
+class QuestionConditionAdmin(
+    DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin
+):
+    class Meta:
+        model = QuestionCondition
+
+
+class SubQuestionAdmin(DisableDeleteAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Question
+
+
+class ResultAdmin(DisableDeleteAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Result
+
+
+class SubQuestionConditionAdmin(
+    DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin
+):
+    class Meta:
+        model = SubQuestionCondition
+
+
+class OptionAdmin(DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Option
+
+
+class AnswerAdmin(DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Answer
+
+
+class PostalCodeAdmin(
+    DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin
+):
+    class Meta:
+        model = PostalCode
+
+
+class PostalCodeTypeAdmin(
+    DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin
+):
+    class Meta:
+        model = PostalCodeType
+
+
+class PostalCodeResultAdmin(
+    DisableDeleteAdminMixin, ReadOnlyFieldsAdminMixin, admin.ModelAdmin
+):
+    class Meta:
+        model = PostalCodeResult
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(QuestionCondition, QuestionConditionAdmin)
+admin.site.register(SubQuestion, SubQuestionAdmin)
+admin.site.register(SubQuestionCondition, SubQuestionConditionAdmin)
+admin.site.register(Option, OptionAdmin)
+admin.site.register(Result, ResultAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(PostalCode, PostalCodeAdmin)
+admin.site.register(PostalCodeType, PostalCodeTypeAdmin)
+admin.site.register(PostalCodeResult, PostalCodeResultAdmin)
