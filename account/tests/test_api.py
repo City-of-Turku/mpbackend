33f3e65a47aa3fd683355bd292432682deb10413
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-16 10:51:49 +0200

    Remove test_email_verify_process

--- account/tests/test_api.py
+++ account/tests/test_api.py
@@ -1,7 +1,4 @@
-import uuid
-
 import pytest
-from django.core import mail
 from rest_framework.reverse import reverse
 
 from account.models import MailingList, MailingListEmail, User
@@ -13,70 +10,11 @@ ALL_METHODS = ("get", "post", "put", "patch", "delete")
 
 @pytest.mark.django_db
 def test_unauthenticated_cannot_do_anything(api_client, users):
-    # TODO, add start-poll url fter recaptcha integration
+    # TODO, add start-poll url after recaptcha integration
     urls = [
         reverse("account:profiles-detail", args=[users.first().id]),
     ]
     check_method_status_codes(api_client, urls, ALL_METHODS, 403)
-
-
-@pytest.mark.django_db
-def test_email_verify_process(api_client):
-    mail.outbox = []
-    test_email = "test@test.com"
-    # Test authentication required
-    url = reverse("account:profiles-email-verify-request") + "?email=test_mail@test.com"
-    response = api_client.get(url)
-    assert response.status_code == 403
-    url = reverse("profiles:question-start-poll")
-    response = api_client.post(url)
-    assert response.status_code == 200
-    assert User.objects.all().count() == 1
-    uid = response.json()["id"]
-    user = User.objects.get(pk=uid)
-    assert user.email_verified is False
-    assert len(mail.outbox) == 0
-    url = reverse("account:profiles-email-verify-request") + f"?email={test_email}"
-    response = api_client.get(url)
-    assert response.status_code == 200
-    assert len(mail.outbox) == 1
-    verify_msg = None
-    for msg in mail.outbox:
-        if "Verify" in msg.subject:
-            verify_msg = msg
-    assert verify_msg
-    # Check contains correct uid
-    uid = str(user.id)
-    assert uid in verify_msg.body
-    # Todo fix when send mail msg is ready
-    token = verify_msg.body.split()[1]
-    url = reverse("account:profiles-email-verify-confirm")
-    response = api_client.post(url, {"uid": uid, "token": token})
-    assert response.status_code == 200
-    user = User.objects.get(pk=uid)
-    assert user.email_verified is True
-    # Try to use the same token
-    response = api_client.post(url, {"uid": uid, "token": token})
-    assert response.status_code == 400
-    # Nonexisting uid and token values
-    response = api_client.post(url, {"uid": uuid.uuid4(), "token": "foobar"})
-    assert response.status_code == 400
-    # Test save email
-    user = User.objects.get(pk=uid)
-    assert user.email is None
-    url = reverse("account:profiles-save-email")
-    response = api_client.post(url, {"email": test_email})
-    assert response.status_code == 201
-    user = User.objects.get(pk=uid)
-    assert user.email == test_email
-    # Test duplicate email
-    url = reverse("profiles:question-end-poll")
-    response = api_client.post(url)
-    url = reverse("profiles:question-start-poll")
-    response = api_client.post(url)
-    url = reverse("account:profiles-save-email")
-    response = api_client.post(url, {"email": test_email})
-    assert response.status_code == 409
 
 
 @pytest.mark.django_db
