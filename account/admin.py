33ded303802dd2814214649c4bd4a55a9bf6d419
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-16 09:58:06 +0200

    Add MailingListAdmin

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.contrib import admin
 
-from .models import Profile, User
+from .models import MailingList, Profile, User
 
 admin.site.register(User)
 
@@ -15,4 +16,39 @@ class ProfileAdmin(admin.ModelAdmin):
             return None
 
 
+class MailingListAdminForm(forms.ModelForm):
+    csv_emails = forms.CharField(widget=forms.Textarea(attrs={"rows": 20, "cols": 120}))
+
+    class Meta:
+        model = MailingList
+        fields = ["result"]
+
+
+class MailingListAdmin(admin.ModelAdmin):
+    list_display = ("result", "csv_emails")
+
+    readonly_fields = ("result",)
+    form = MailingListAdminForm
+
+    def csv_emails(self, obj):
+        return obj.csv_emails
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["csv_emails"].initial = self.csv_emails(obj)
+        return form
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["show_save_and_add_another"] = False
+        extra_context["show_save_and_continue"] = False
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context
+        )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(MailingList, MailingListAdmin)
