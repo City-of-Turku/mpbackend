f21853c188e737dea263bd480ddea76007ebc308
juuso-j <juuso.jokiniemi@turku.fi>
2024-01-16 10:07:24 +0200

    Add endpoint to subscribe and unsubscribe to mailing list

--- account/api/views.py
+++ account/api/views.py
@@ -1,20 +1,24 @@
 import datetime
 
+from django import db
 from django.conf import settings
 from django.contrib.auth import get_user, get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
+from django.core.validators import validate_email
 from django.utils import timezone
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from account.models import Profile, User
+from account.models import MailingList, MailingListEmail, Profile, User
+from profiles.models import Result
 
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, SubscribeSerializer, UnSubscribeSerializer
 
 all_views = []
 
@@ -26,7 +30,7 @@ class ProfileViewSet(UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = ProfileSerializer
 
     def get_permissions(self):
-        if self.action in ["email_verify_confirm"]:
+        if self.action in ["unsubscribe", "email_verify_confirm"]:
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsAuthenticated]
@@ -44,6 +48,72 @@ class ProfileViewSet(UpdateModelMixin, viewsets.GenericViewSet):
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        description="Subscribe the email to the mailing list attached to the result.",
+        request=SubscribeSerializer,
+        responses={
+            201: OpenApiResponse(description="subscribed"),
+            400: OpenApiResponse(
+                description="Validation error, detailed information in response"
+            ),
+        },
+    )
+    @action(
+        detail=False,
+        methods=["POST"],
+        permission_classes=[IsAuthenticated],
+    )
+    @db.transaction.atomic
+    def subscribe(self, request):
+        result = Result.objects.filter(id=request.data.get("result", None)).first()
+        if not result:
+            return Response("'result' not found", status=status.HTTP_400_BAD_REQUEST)
+
+        email = request.data.get("email", None)
+        if not email:
+            return Response("No 'email' provided", status=status.HTTP_400_BAD_REQUEST)
+        if MailingListEmail.objects.filter(email=email).count() > 0:
+            return Response("'email' exists", status=status.HTTP_400_BAD_REQUEST)
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            return Response(f"Invalid email: {e}", status=status.HTTP_400_BAD_REQUEST)
+        mailing_list = MailingList.objects.filter(result=result).first()
+        if not mailing_list:
+            # In case mailing list is not created for the result, it is created.
+            mailing_list = MailingList.objects.create(result=result)
+
+        MailingListEmail.objects.create(mailing_list=mailing_list, email=email)
+        return Response("subscribed", status=status.HTTP_201_CREATED)
+
+    @extend_schema(
+        description="Unaubscribe the email from the mailing list attached to the result.",
+        request=UnSubscribeSerializer,
+        responses={
+            200: OpenApiResponse(description="unsubscribed"),
+            400: OpenApiResponse(
+                description="Validation error, detailed information in response"
+            ),
+        },
+    )
+    @action(
+        detail=False,
+        methods=["POST"],
+        permission_classes=[AllowAny],
+    )
+    def unsubscribe(self, request):
+        email = request.data.get("email", None)
+        if not email:
+            return Response("No 'email' provided", status=status.HTTP_400_BAD_REQUEST)
+        qs = MailingListEmail.objects.filter(email=email)
+        if not qs.exists():
+            return Response(
+                f"{email} does not exists", status=status.HTTP_400_BAD_REQUEST
+            )
+
+        qs.delete()
+        return Response(f"unsubscribed {email}", status=status.HTTP_200_OK)
 
     @action(
         detail=False,
