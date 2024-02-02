a9a83125c8dfbd2090baa6f21c025de833ed6413
juuso-j <juuso.jokiniemi@turku.fi>
2024-02-02 13:03:15 +0200

    Remove obsolete urls

--- mpbackend/urls.py
+++ mpbackend/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_spectacular.views import (
@@ -8,13 +9,16 @@ from drf_spectacular.views import (
 
 import account.api.urls
 import profiles.api.urls
-from profiles.views import get_csrf
 
 urlpatterns = [
     re_path("^admin/", admin.site.urls),
-    # re_path(r"^api/v1/", include(router.urls)),
     re_path(r"^api/account/", include(account.api.urls), name="account"),
     re_path(r"^api/v1/", include(profiles.api.urls), name="profiles"),
+    # path("api-auth/", include("rest_framework.urls")),
+]
+
+# NOTE, consider disabling in production. if settings.DEBUG:
+urlpatterns += [
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/v1/schema/swagger-ui/",
@@ -26,6 +30,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("csrf/", get_csrf)
-    # path("api-auth/", include("rest_framework.urls")),
 ]
