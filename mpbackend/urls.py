bda7d912d06a1f8eec9ae27075ecbac4f75c143b
juuso-j <juuso.jokiniemi@turku.fi>
2024-02-02 13:16:44 +0200

    Remove unused import

--- mpbackend/urls.py
+++ mpbackend/urls.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+# from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_spectacular.views import (
