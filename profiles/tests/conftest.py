041d289f7acb58642e5de3f2a7bb2f049bf5e0db
juuso-j <juuso.jokiniemi@turku.fi>
2024-02-08 10:44:02 +0200

    Add other option fixture

--- profiles/tests/conftest.py
+++ profiles/tests/conftest.py
@@ -42,9 +42,7 @@ def questions():
         question="How often do you use following means of public transport?", number="2"
     )
     Question.objects.create(question="Why do you use train?", number="3")
-
     Question.objects.create(question="Questions about car driving", number="4")
-
     return Question.objects.all()
 
 
@@ -58,6 +56,7 @@ def sub_questions(questions):
     SubQuestion.objects.create(
         question=question, description="Do you drive yourself?", order_number=0
     )
+
     return SubQuestion.objects.all()
 
 
@@ -71,7 +70,6 @@ def options(questions, sub_questions, results):
     option_no.results.add(negative_result)
     option_yes = Option.objects.create(value="yes", question=question1)
     option_yes.results.add(positive_result)
-
     train_sub_q = sub_questions.get(description="train")
     car_sub_q = sub_questions.get(description="car")
     Option.objects.create(value="never", sub_question=train_sub_q)
@@ -83,6 +81,8 @@ def options(questions, sub_questions, results):
     question3 = Question.objects.get(number="3")
     Option.objects.create(value="fast", question=question3)
     Option.objects.create(value="easy", question=question3)
+    Option.objects.create(value="other", question=question3, is_other=True)
+
     Option.objects.create(
         value="yes I drive",
         sub_question=SubQuestion.objects.get(description="Do you drive yourself?"),
@@ -147,6 +147,9 @@ def users():
     Profile.objects.create(user=user)
     user = User.objects.create(username="car and train user")
     Profile.objects.create(user=user)
+    user = User.objects.create(username="no answers user")
+    Profile.objects.create(user=user)
+
     return User.objects.all()
 
 
